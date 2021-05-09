cd7148346704912f0184e6b396c8815315dea811
Ryan-Choy <ryandchoy@gmail.com>
2021-05-09 11:28:40 -0700

    project one upload

---
+++ projectonereinbursement/src/main/java/com/revature/models/Reinbursement.java
@@ -0,0 +1,14 @@
+package com.revature.models;
+
+public class Reinbursement {
+	private Integer reimbId;
+	private Double reimbAmount;
+	private String reimbSubmit;
+	private String reimbResolve;
+	private String reimbDescript;
+	private Integer reimbAuthor;
+	private Integer reimbStatusId;
+	
+	
+
+}
---
+++ projectonereinbursement/src/main/java/com/revature/models/User.java
@@ -0,0 +1,13 @@
+package com.revature.models;
+
+public class User {
+	private Integer userId;
+	private String userRole;
+	private String userName;
+	private String passWord;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+
+}
